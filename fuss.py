581c908ba25befcb3efc849192237a2a6565d404
lena-pa1 <96227687+lena-pa1@users.noreply.github.com>
2021-12-16 08:56:05 +0100

    Update fuss.py

--- fuss.py
+++ fuss.py
@@ -1,3 +1,5 @@
+# Dieser Code rechnet deine Grösse von Centimeter in Fuss
+
 Grösse = input ("Gib deine Grösse in Centimeter ein: ")
 feet = float(Grösse) * 0.0328084
 print("Deine Grösse in Fuss: " + str(feet))
